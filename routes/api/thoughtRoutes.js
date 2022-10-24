644041957716cbd690dd30f8d0a4a79e50a15773
BrianZoulko <bzoulko@gmail.com>
2022-10-23 22:15:30 -0700

    Updating for the night.

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,15 +1,146 @@
 const router = require('express').Router();
+const User = require('../../models/users');
 const Thought = require('../../models/thoughts');
+const Reaction = require('../../models/reactions');
 
-// Find ALL Thoughts.
+
+// GET (FIND) ALL Thoughts.
 router.get('/', (req, res) => {
+    const errMsg = `{ msg: Nothing found! }`;
+
     Thought.find({}, (err, result) => {
         if (result) {
             res.status(200).json(result);
         } else {
-            res.status(500).send({ message: '(Thought Model) Internal Server Error' });
+            res.status(500).send(errMsg);
         }
     });
 });
+
+
+// GET (FIND) a single user.
+router.get('/:_id', (req, res) => {
+    const id = req.params._id;
+    const errMsg = `{ msg: '${id}' - Not found! }`;
+
+    if (id.length == 12 || id.length == 24) {
+        Thought.findOne({_id: id }, function(err, user) {
+            if (err) res.status(500).json(err + " " + errMsg);            
+            res.status(200).json(user);
+        });
+    } else {
+        res.json(errMsg);
+    }
+});
+
+
+// POST (ADD) one or more thoughts.
+router.post('/', (req, res) => {
+
+    let body = JSON.stringify(req.body);
+    let errMsg = `{ msg: Unable to ADD: ${body} }`;
+    let thoughts = JSON.parse(body);
+    
+    // Determine when more than one is being posted.
+    if (thoughts.length) {
+        Thought.insertMany(thoughts)
+            .then((result) => {
+                UpdateAllUsersThoughts(thoughts);
+                res.send(result);
+            })
+            .catch((err) => res.json(err + " " + errMsg));
+    } else {        
+        Thought.create(thoughts)
+            .then((result) => {
+                UpdateAUsersThought(thoughts);
+                res.json(result)
+            })
+            .catch((err) => res.json(err + " " + errMsg));
+    }
+
+});
+
+
+// Update all associated users thoughts.
+function UpdateAllUsersThoughts(thoughts) {
+    // Update all thoughts to all associated users.
+    for (thought in thoughts) {
+        UpdateAUsersThought(thought);
+    }
+}
+
+
+// Update an associated users thought.
+function UpdateAUsersThought(thought) {
+    let userId = thought.userId;
+
+    // Make sure input Id's meet the required length and locate the user.
+    if (userId.length == 12 || userId.length == 24) {
+        User.findOne({_id: userId }, function(err, user) {
+
+            // Check for errors, if none, append to parent.
+            if (err) res.json(err + " " + errMsg);            
+            user.thoughts.push(thoughtId);
+
+            // Update user with new a thought.
+            User.updateOne({ _id: userId }, { $set: user } )
+                .catch((err) => res.json(err + " " + errMsg));
+
+        });
+    }
+
+}
+
+
+// POST (UPDATE) a reaction in a thought.
+router.post('/:thoughtId/reactions', (req, res) => {
+
+    // Define input variables from POST.
+    let thoughtId = req.params.thoughtId;
+    let reactions = req.body;
+    let errMsg = `{ msg: Unable to UPDATE: ${"Thought - " + thoughtId + "  w/Reactions - " + JSON.stringify(reactions)} }`;
+    
+    // Make sure input Id's meet the required length and locate the user.
+    if (reactions) {        
+        Thought.findOne({_id: thoughtId }, function(err, thought) {
+
+            // Check for errors, if none, append to parent.
+            if (err) res.json(err + " " + errMsg);
+            thought.reactions.push(...reactions);
+
+            // Update thought with new reaction.
+            Reaction.updateOne({ _id: userId }, { $set: user })
+                .then((result) => res.json(result))
+                .catch((err) => res.json(err + " " + errMsg));
+
+        });
+    } else {
+        res.json(errMsg);
+    }
+
+});
+
+
+// PUT (UPDATE) thought by its _id.
+router.put('/:_id', (req, res) => {
+    let id = req.params._id;
+    let errMsg = `{ msg: Unable to UPDATE: '${id}' }`;
+
+    Thought.updateOne({ _id: id }, { $set: req.body } )
+        .then((result) => res.json(result))
+        .catch((err) => res.json(err + " " + errMsg));
+});
+
+
+// DELETE - thought by _id.
+router.delete('/:_id', (req, res) => {
+    let id = req.params._id;
+    let errMsg = `{ msg: Unable to DELETE: '${id}' }`;
+
+    Thought.deleteOne({ _id: id })
+        .then((result) => res.json(result))
+        .catch((err) => res.json(err + " " + errMsg));
+});
+
 
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const User = require('../../models/users');
 
-// GET all users.
+
+// GET (FIND) all users.
 router.get('/', (req, res) => {
     const errMsg = `{ msg: Nothing found! }`;
 
@@ -14,25 +15,24 @@ router.get('/', (req, res) => {
     });
 });
 
-// GET a single user.
+
+// GET (FIND) a single user.
 router.get('/:_id', (req, res) => {
     const id = req.params._id;
     const errMsg = `{ msg: '${id}' - Not found! }`;
 
     if (id.length == 12 || id.length == 24) {
-        User.find({_id: id }, (err, result) => {
-            if (result.length) {
-                res.status(200).json(result);
-            } else {
-                res.status(500).send(errMsg);
-            }
+        User.findOne({_id: id }, function(err, user) {
+            if (err) res.status(500).json(err + " " + errMsg);            
+            res.status(200).json(user);
         });
     } else {
         res.json(errMsg);
     }
 });
 
-// POST a new user.
+
+// POST (ADD) one or more users.
 router.post('/', (req, res) => {
     // Create JSON array for bulk create when posting muliple.
     let body = JSON.stringify(req.body);
@@ -52,34 +52,27 @@ router.post('/', (req, res) => {
 });
 
 
-// POST a new friend to a user.
+// POST (UPDATE) a new friend to a user.
 router.post('/:userId/friends/:friendId', (req, res) => {
-    // Create JSON array for bulk create when posting muliple.
+
+    // Define input variables from POST.
     let friendId = req.params.friendId;
     let userId = req.params.userId;
     let errMsg = `{ msg: Unable to ADD: ${"User - " + userId + "  new Friend - " + friendId} }`;
-
+    
+    // Make sure input Id's meet the required length and locate the user.
     if (userId.length == 12 || userId.length == 24) {
-        User.find({_id: userId }, (err, user) => {
-            if (user.length) {
-                User.find({_id: userId }, (err, user) => {
-                    if (user.length) {
-                        // Determine when more than one is being posted.
-                        User.create(user)
-                            .then((result) => res.json(result))
-                            .catch((err) => res.json(err + " " + errMsg));
-                        
-                        User.updateOne({ _id: id }, { $set: req.body } )
-                            .then((result) => res.json(result))
-                            .catch((err) => res.json(err + " " + errMsg));
-        
-                    } else {
-                        res.status(500).send(err + " " + errMsg);
-                    }
-                });
-                    } else {
-                res.status(500).send(err + " " + errMsg);
-            }
+        User.findOne({_id: userId }, function(err, user) {
+
+            // Check for errors, if none, append to parent.
+            if (err) res.json(err + " " + errMsg);            
+            user.friends.push(friendId);
+
+            // Update user with new a friend.
+            User.updateOne({ _id: userId }, { $set: user } )
+                .then((result) => res.json(result))
+                .catch((err) => res.json(err + " " + errMsg));
+
         });
     } else {
         res.json(errMsg);
@@ -88,7 +81,36 @@ router.post('/:userId/friends/:friendId', (req, res) => {
 });
 
 
-// Updates a user by its _id.
+// POST (UPDATE) a new Thought to a user.
+router.post('/:userId/thoughts/:thoughtId', (req, res) => {
+
+    // Define input variables from POST.
+    let thoughtId = req.params.thoughtId;
+    let userId = req.params.userId;
+    let errMsg = `{ msg: Unable to ADD: ${"User - " + userId + "  new Thought - " + thoughtId} }`;
+    
+    // Make sure input Id's meet the required length and locate the user.
+    if (userId.length == 12 || userId.length == 24) {
+        User.findOne({_id: userId }, function(err, user) {
+
+            // Check for errors, if none, append to parent.
+            if (err) res.json(err + " " + errMsg);            
+            user.thoughts.push(thoughtId);
+
+            // Update user with new a thought.
+            User.updateOne({ _id: userId }, { $set: user } )
+                .then((result) => res.json(result))
+                .catch((err) => res.json(err + " " + errMsg));
+
+        });
+    } else {
+        res.json(errMsg);
+    }
+
+});
+
+
+// PUT (UPDATE) user by its _id.
 router.put('/:_id', (req, res) => {
     let id = req.params._id;
     let errMsg = `{ msg: Unable to UPDATE: '${id}' }`;
@@ -99,7 +121,7 @@ router.put('/:_id', (req, res) => {
 });
 
 
-// Delete user by _id.
+// DELETE - user by _id.
 router.delete('/:_id', (req, res) => {
     let id = req.params._id;
     let errMsg = `{ msg: Unable to DELETE: '${id}' }`;
@@ -108,5 +130,80 @@ router.delete('/:_id', (req, res) => {
         .then((result) => res.json(result))
         .catch((err) => res.json(err + " " + errMsg));
 });
-  
+
+
+// DELETE - friend from a user.
+router.delete('/:userId/friends/:friendId', (req, res) => {
+
+    // Define input variables from POST.
+    let friendId = req.params.friendId;
+    let userId = req.params.userId;
+    let errMsg = `{ msg: Unable to DELETE: ${"User - " + userId + "  new Friend - " + friendId} }`;
+
+    // Make sure input Id's meet the required length and locate the user.
+    if (userId.length == 12 || userId.length == 24) {
+        User.findOne({ _id: userId }, function (err, user) {
+
+            // Check for errors, if none, append to parent.
+            if (err) res.json(err + " " + errMsg);
+            
+            // Locate index of item to remove.
+            const index = user.friends.indexOf(friendId);
+
+            if (index > -1) {
+                user.friends.splice(index, 1); 
+
+                // Update user with new friends list
+                User.updateOne({ _id: userId }, { $set: user })
+                    .then((result) => res.json(result))
+                    .catch((err) => res.json(err + " " + errMsg));                    
+            } else {
+                res.json(errMsg);
+            }
+
+        });
+    } else {
+        res.json(errMsg);
+    }
+
+});
+
+
+// DELETE - thought from a user.
+router.delete('/:userId/thoughts/:thoughtId', (req, res) => {
+
+    // Define input variables from POST.
+    let thoughtId = req.params.thoughtId;
+    let userId = req.params.userId;
+    let errMsg = `{ msg: Unable to DELETE: ${"User - " + userId + "  new Thought - " + thoughtId} }`;
+
+    // Make sure input Id's meet the required length and locate the user.
+    if (userId.length == 12 || userId.length == 24) {
+        User.findOne({ _id: userId }, function (err, user) {
+
+            // Check for errors, if none, append to parent.
+            if (err) res.json(err + " " + errMsg);
+            
+            // Locate index of item to remove.
+            const index = user.thoughts.indexOf(thoughtId);
+
+            if (index > -1) {
+                user.thoughts.splice(index, 1); 
+
+                // Update user with new thought list
+                User.updateOne({ _id: userId }, { $set: user })
+                    .then((result) => res.json(result))
+                    .catch((err) => res.json(err + " " + errMsg));                    
+            } else {
+                res.json(errMsg);
+            }
+
+        });
+    } else {
+        res.json(errMsg);
+    }
+
+});
+
+
 module.exports = router;
